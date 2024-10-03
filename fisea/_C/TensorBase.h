ff9f7a10c0c99d33f301f520606f10fda4f0c787
See.Looooo <siumabon123@gmail.com>
2024-10-03 11:50:42 +0800

    Update TensorBase.h

--- fisea/_C/TensorBase.h
+++ fisea/_C/TensorBase.h
@@ -1,7 +1,37 @@
 // Tensor的基類, 定義了非常多虛函數方法.
 
 #pragma once
+#include <memory>
+#include <iostream>
+#include <vector>
+
+#include "type.h"
 
 namespace fisea {
+    class TensorBase {
+    private:
+        std::vector<int> shape;
+        std::vector<int> stride;
+        
+        fisea::Device device;
+        fisea::Dtype dtype;
 
+        std::shared_ptr<void> data = nullptr;
+
+        bool requires_grad = false;
+        TensorBase *grad = nullptr;
+        // grad_fn
+        bool is_leaf = false;
+        
+
+    public:
+        ~TensorBase() {
+            std::cout << "[DEBUG] Tensor has been removed" << std::endl;
+        }
+        // static TensorBase *create(const std::vector<int> &shap);
+        virtual void initialize() = 0;
+        virtual void finalize() = 0;
+        virtual void reshape(int new_shape) = 0;
+        virtual void print() const = 0;
+    };
 }
