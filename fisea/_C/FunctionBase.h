757fbaa3ae573f63541523142f37cbb16bf4f82e
Season Lo <siumabon123@gmail.com>
2024-10-04 20:55:14 +0800

    add

--- fisea/_C/FunctionBase.h
+++ fisea/_C/FunctionBase.h
@@ -1,4 +1,50 @@
+#include <iostream>
 #pragma once
 
 namespace fisea {
-}
+    template <typename Dtype>
+    class Function
+    {
+    protected:
+        virtual void forward(std::shared_ptr<Tensor> input, std::shared_ptr<Tensor> output = nullptr) = 0;
+        virtual void backward_cal(std::shared_ptr<Tensor> input, std::shared_ptr<Tensor> gradient, std::shared_ptr<Tensor> output = nullptr) = 0;
+
+    public:
+        virtual std::shared_ptr<Tensor> backward(std::shared_ptr<Tensor> input, std::shared_ptr<Tensor> gradient) = 0;
+        virtual void backward(std::shared_ptr<Tensor> input, std::shared_ptr<Tensor> gradient, std::shared_ptr<Tensor> output) = 0;
+        virtual std::shared_ptr<Tensor> operator()(std::shared_ptr<Tensor> input) {
+            forward(input);
+            return input;
+        }
+        virtual void operator()(std::shared_ptr<Tensor> input, std::shared_ptr<Tensor> output){
+            forward(input, output);
+        }
+    };
+    
+    template <typename Dtype>
+    class ReLU : public Function<Dtype>
+    {
+    protected:
+        void forward(std::shared_ptr<Tensor> input, std::shared_ptr<Tensor> output = nullptr);
+        void backward_cal(std::shared_ptr<Tensor> input, std::shared_ptr<Tensor> gradient, std::shared_ptr<Tensor> output = nullptr);
+
+    public:
+        ReLU();
+        ~ReLU();
+        static Function<Dtype>* create();
+        std::shared_ptr<Tensor> backward(std::shared_ptr<Tensor> input, std::shared_ptr<Tensor> gradient);
+        void backward(std::shared_ptr<Tensor> input, std::shared_ptr<Tensor> gradient, std::shared_ptr<Tensor> output);
+    };
+    
+    template <typename Dtype>
+    ReLU<Dtype>::ReLU(){}
+    template <typename Dtype>
+    ReLU<Dtype>::~ReLU(){}
+    template <typename Dtype>
+    Function<Dtype>* ReLU<Dtype>::create(){
+        return new ReLU<Dtype>();
+    }
+
+
+
+} // namespace fisea
--- fisea/_C/main.cpp
+++ fisea/_C/main.cpp
@@ -2,13 +2,75 @@
 
 #include <iostream>
 #include <vector>
-#include "functional/kernel.cuh"
-#include "tensor/Tensor.h"
+#include <typeinfo>
+   class testA : public std::enable_shared_from_this<testA>
+    {
+    public:
 
-int main() {
-    std::vector<int> shape = {2, 3, 4};
-    fisea::Tensor t = fisea::Tensor(shape, "cpu", "float");
-    t.cpu();
-    // t.cuda();
+        virtual void print() = 0;
+        virtual ~testA() {}
+        template <typename T>
+        T getdata()
+        {
+            const testB *b = dynamic_cast<const testB *>(this);
+            if (b)
+            {
+                return b->data;
+            }
+            return 0;
+        }
+        
+    };
+    class testB : public testA, public std::enable_shared_from_this<testB>
+    {
+        private:
+        float data = 1.5;
+    protected:
+        float data2 = 2.5;
+    public:
+        int a = 10;
+        friend class testA;
+        testB();
+        ~testB();
+        void print();
+        float getdata() const;
+        static std::shared_ptr<testA> create();
+        
+    };
+
+    testB::testB()
+    {
+        std::cout << "testB" << std::endl;
+    }
+    testB::~testB()
+    {
+        std::cout << "~testB" << std::endl;
+    }
+    void testB::print()
+    {
+        std::cout << "print" << std::endl;
+    }
+    std::shared_ptr<testA> testB::create()
+    {
+        std::cout << "create" << std::endl;
+        
+        return std::make_shared<testB>();
+    }
+    float testB::getdata() const
+    {
+        return data;
+    }
+int main()
+{
+    // std::vector<int> shape = {2, 3, 4};
+    // fisea::Tensor t = fisea::Tensor(shape, "cpu", "float");
+    // t.cpu();
+    // // t.cuda();
+    // return 0;
+    std::shared_ptr<testA> a = testB::create();
+    // std::cout << a->getdata<float>() << std::endl;
+    a->print();
+    std::cout << a->getdata<float>() << std::endl;
+    
     return 0;
 }
